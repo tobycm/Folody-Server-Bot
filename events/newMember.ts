afb223d037826b9619f6d37770420e5725783067
Toby <newtobycm@gmail.com>
2023-08-02 20:32:42 -0700

    update name

--- events/newMember.ts
+++ events/newMember.ts
@@ -17,8 +17,20 @@ async function onGuildMemberAdd(member: GuildMember) {
   ) as GuildTextBasedChannel;
 
   if (!welcomeChannel) return;
-  await welcomeChannel.send({
-    content: `<:pepehooray:1125131846267711488> ${member.user}\n**<:folody:933915537233289336> Hi there!**\n- Please read ⁠<#955639800168316998>\n- Hope you have a good time in our server\n\n<:hithere:1131104394750984202> <@&993522874897940540> | <@&960561360624840724> Say hi to new member <:hithere:1131104394750984202>\nThere's a \`${member.guild.memberCount}\` member coming in <:whatsup:1124364311230431293>`,
+
+  if (member.user.bot)
+    return welcomeChannel.send(
+      `Có con npc ${member} vào si vi kìa men :scream: :scream: :scream:`
+    );
+
+  welcomeChannel.send({
+    content:
+      `<:pepehooray:1125131846267711488> ${member.user}\n` +
+      `**<:folody:933915537233289336> Hi there!**\n` +
+      `- Please read <#955639800168316998>\n` +
+      `- Hope you have a good time in our server\n\n` +
+      `<:hithere:1131104394750984202> <@&993522874897940540> | <@&960561360624840724> Say hi to new member <:hithere:1131104394750984202>\n` +
+      `There's a \`${member.guild.memberCount}\` member coming in <:whatsup:1124364311230431293>`,
     files: [
       new AttachmentBuilder(await WelcomeCard(member), {
         name: "welcome.png",
