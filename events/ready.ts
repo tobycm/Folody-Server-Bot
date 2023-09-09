f74566ce44bc48ebe913438ecf106fab3588399e
Toby <62174797+tobycm@users.noreply.github.com>
2023-09-09 09:49:37 -0700

    fix: :fire: activity bủh quá

--- events/ready.ts
+++ events/ready.ts
@@ -4,17 +4,7 @@ import Event from "modules/event";
 async function onReady(folody: Client) {
   console.log(`Logged in as ${folody.user!.tag}!`);
 
-  // activity case
-  setTimeout(() => {
-    const statuses = [
-      `<:folody:933915537233289336> Folody Studio | f!help`,
-      `<:leuleu:1135840069257805895> f!help for help | f!help`,
-      `<:mrStorm:1120554787298086943> chat to become Chad | f!help`,
-    ];
-
-    const status = statuses[Math.floor(Math.random() * statuses.length)];
-    folody.user?.setActivity(status, { type: ActivityType.Listening });
-  }, 2000);
+  folody.user!.setActivity("with Toby", { type: ActivityType.Playing });
 }
 
 export default new Event({
