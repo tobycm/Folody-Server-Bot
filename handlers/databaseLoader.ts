4f782ccdae2276a72b00e2ddf0bf1381ececa53c
Toby <62174797+tobycm@users.noreply.github.com>
2023-09-11 15:56:07 -0700

    fix: :ambulance: Driver commit trc het cuu qua

--- handlers/databaseLoader.ts
+++ handlers/databaseLoader.ts
@@ -3,14 +3,27 @@ import { QuickDB } from "quick.db";
 
 const db = new Database("./data/database.sqlite");
 
-interface Row {
-  id: string;
-  value: any;
-}
-
 class BunDBDriver {
-  async getAllRows(table: string): Promise<Row[]> {
-    return db.query(`SELECT * FROM ${table}`).all() as Row[];
+  async prepare(table: string) {
+    db.query(
+      `CREATE TABLE IF NOT EXISTS ${table} (ID TEXT PRIMARY KEY, json TEXT)`
+    ).run();
+  }
+
+  async getAllRows(table: string): Promise<
+    {
+      id: string;
+      value: any;
+    }[]
+  > {
+    const rows = db.query(`SELECT * FROM ${table}`).all() as {
+      ID: string;
+      json: string;
+    }[];
+    return rows.map((row) => ({
+      id: row.ID,
+      value: JSON.parse(row.json),
+    }));
   }
 
   async getRowByKey<T>(
@@ -18,8 +31,8 @@ class BunDBDriver {
     key: string
   ): Promise<[T | null, boolean]> {
     const value = db
-      .query(`SELECT json FROM ${table} WHERE ID = ${key}`)
-      .all()[0] as { id: string; json: string };
+      .prepare(`SELECT json FROM ${table} WHERE ID = ?`)
+      .all(key)[0] as { id: string; json: string };
     return value != undefined
       ? [JSON.parse(value.json) as T, true]
       : [null, false];
@@ -52,12 +65,8 @@ class BunDBDriver {
   }
 
   async deleteRowByKey(table: string, key: string): Promise<number> {
-    db.query(`DELETE FROM ${table} WHERE id = ${key}`).run();
+    db.prepare(`DELETE FROM ${table} WHERE id = ?`).run(key);
     return 0;
-  }
-
-  async prepare(table: string) {
-    db.query(`CREATE TABLE IF NOT EXISTS ${table} (id TEXT, value TEXT)`).run();
   }
 }
 
