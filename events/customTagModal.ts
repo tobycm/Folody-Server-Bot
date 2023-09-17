76d288b078c20c908647dfdde7a325627bf8e4fd
Toby <62174797+tobycm@users.noreply.github.com>
2023-09-16 17:45:19 -0700

    fix: :ambulance: chưa import xd

--- events/customTagModal.ts
+++ events/customTagModal.ts
@@ -1,30 +1,31 @@
 import Folody from "Folody";
-import { Events } from "discord.js";
+import { Events, inlineCode } from "discord.js";
 import Event from "modules/event";
-import { BaseExceptions, GuildExceptions } from "modules/exceptions";
+import CustomTags from "modules/models/customTags";
 
 export default new Event({
   eventName: Events.InteractionCreate,
   async run(interaction) {
     if (!interaction.isModalSubmit()) return;
-    if (interaction.customId === 'tag_modal') {
+    if (interaction.customId === "tag_modal") {
+      const folody = interaction.client as Folody;
+      const tag = interaction.fields.getTextInputValue("tag_name");
+      const content = interaction.fields.getTextInputValue("tag_content");
 
-    const folody = interaction.client as Folody;
-    const tag = interaction.fields.getTextInputValue('tag_name');
-	  const content = interaction.fields.getTextInputValue('tag_content');
+      const customTags =
+        (await folody.db.get<CustomTags>(
+          `${interaction.guild!.id}.customTags`
+        )) || {};
 
-    const customTags =
-      (await folody.db.get<CustomTags>(
-        `${interaction.guild!.id}.customTags`
-      )) || {};
+      customTags[tag] = { content, author: interaction.user.id };
 
-    customTags[tag] = { content, author: interaction.user.id };
-
-    folody.db.set<CustomTags>(
-      `${interaction.guild!.id}.customTags`,
-      customTags
-    );
-    return interaction.reply({ content: `Đã tạo tag ${inlineCode(tag)} thành công!` });
-    };
+      folody.db.set<CustomTags>(
+        `${interaction.guild!.id}.customTags`,
+        customTags
+      );
+      return interaction.reply({
+        content: `Đã tạo tag ${inlineCode(tag)} thành công!`,
+      });
+    }
   },
 });
