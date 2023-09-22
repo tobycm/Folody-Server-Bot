2978b26f951c6393bdeff507fa7bfe3214f84cda
Toby <toby@vsus.app>
2023-09-22 09:54:24 -0700

    feat: :sparkles: chắc ổn :>

--- events/getTikTokVideo.ts
+++ events/getTikTokVideo.ts
@@ -1,7 +1,24 @@
-import { Events } from "discord.js";
+import { AttachmentBuilder, Events } from "discord.js";
+import { downloadOne } from "modules/TikTokDownloader";
 import Event from "modules/event";
 
 export default new Event({
   eventName: Events.MessageCreate,
-  async run(message) {},
+  async run(message) {
+    // ko bt regex có ổn ko
+    const tiktokRegex =
+      /(@[a-zA-z0-9]*|.*)(\/.*\/|trending.?shareId=)([\d]*)/gm;
+
+    const matches = message.content.match(tiktokRegex);
+
+    if (!matches) return;
+
+    const id = matches[3];
+
+    message.channel.send({
+      files: [
+        new AttachmentBuilder(await downloadOne(id), { name: `${id}.mp4` }),
+      ],
+    });
+  },
 });
--- modules/TikTokDownloader/index.ts
+++ modules/TikTokDownloader/index.ts
@@ -15,43 +15,21 @@ headersWm.append(
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
 );
 
-async function downloadMedia(url: string): Promise<ArrayBuffer> {
-  return (await fetch(url)).arrayBuffer();
+async function downloadMedia(url: string): Promise<Buffer> {
+  return Buffer.from(await (await fetch(url)).arrayBuffer());
 }
 
-async function getVideoNoWM(url: string): Promise<{ url: string; id: string }> {
-  const idVideo = getVideoId(url);
-  return {
-    url: (
-      await (
-        await fetch(
-          `https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${idVideo}`,
-          { method: "GET", headers: headers }
-        )
-      ).json()
-    ).aweme_list[0].video.play_addr.url_list[0],
-    id: idVideo,
-  };
+async function getVideoNoWM(id: string): Promise<string> {
+  return await (
+    await (
+      await fetch(
+        `https://api16-normal-c-useast1a.tiktokv.com/aweme/v1/feed/?aweme_id=${id}`,
+        { method: "GET", headers: headers }
+      )
+    ).json()
+  ).aweme_list[0].video.play_addr.url_list[0];
 }
 
-async function getRedirectUrl(url: string): Promise<string> {
-  if (url.includes("vm.tiktok.com") || url.includes("vt.tiktok.com"))
-    url = (await (await fetch(url, { redirect: "follow" })).json()).url;
-
-  return url;
-}
-
-function getVideoId(url: string): string {
-  if (!url.includes("/video/")) throw new Error("Invalid TikTok video URL");
-
-  const idVideo = url.substring(url.indexOf("/video/") + 7, url.length);
-  return idVideo.length > 19
-    ? idVideo.substring(0, idVideo.indexOf("?"))
-    : idVideo;
-}
-
-export async function downloadOne(url: string): Promise<ArrayBuffer> {
-  return await downloadMedia(
-    (await getVideoNoWM(await getRedirectUrl(url))).url
-  );
+export async function downloadOne(id: string): Promise<Buffer> {
+  return await downloadMedia(await getVideoNoWM(id));
 }
