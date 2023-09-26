0bb6de07a379edf2b4c3f6ccaa3e8c1a861d830d
Kravon Codetech <87592914+KravonCodetech@users.noreply.github.com>
2023-09-26 14:56:23 +0700

    Update donate msg

--- events/messageCommand.ts
+++ events/messageCommand.ts
@@ -21,6 +21,11 @@ export default new Event({
 
     const prefix = await folody.getPrefix(message.guild.id);
 
+    if (message.content.startsWith('ogive') || message.content.startsWith('owogive')) {
+      if (!(message.content.split(' '))[1] || isNan(Number((message.content.split(' '))[2])) || !(message.content.split(' '))[2]) return
+      else if ((message.content.split(' '))[1] === '<@926643835419910184>') return message.reply(`> <a:nhoamnhoam:1145729266517016606> cảm ơn bạn ${message.member} đã donate cho bọn mình **_${(message.content.split(' '))[2]}_ cowoncy** nha!`)
+    }
+    
     if (message.content == userMention(folody.user.id))
       return message.reply(
         `Prefix của bot là ${inlineCode(
