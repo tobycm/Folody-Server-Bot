5e5a57f84debd3702a47ecbe07d77f1fbf9fa936
Toby <toby@vsus.app>
2023-09-29 19:53:19 -0700

    fix: :ambulance: regex siuuuuuu

--- events/getTikTokVideo.ts
+++ events/getTikTokVideo.ts
@@ -2,22 +2,23 @@ import { AttachmentBuilder, Events } from "discord.js";
 import { downloadOne } from "modules/TikTokDownloader";
 import Event from "modules/event";
 
+const tiktokRegex =
+  /^https:\/\/(www|v[a-z]{1}|[a-z])+\.(tiktok|tiktokv)\.com\/@?(\w.+)\//;
+
 export default new Event({
   eventName: Events.MessageUpdate,
   async run(message) {
     if (!message.embeds[0]) return;
-    if (!message.embeds[0].url) return;
 
-    const tiktokRegex = /https:\/\/www\.tiktok\.com\/@[^/]+\/video\/(\d+)/;
+    const url = message.embeds[0].url;
 
-    const matches = message.embeds[0].url.match(tiktokRegex);
-    if (!matches) return;
+    if (!url) return;
+
+    if (!url.match(tiktokRegex)) return;
 
     message.channel.send({
       files: [
-        new AttachmentBuilder(await downloadOne(message.embeds[0].url), {
-          name: "tiktok.mp4",
-        }),
+        new AttachmentBuilder(await downloadOne(url), { name: "tiktok.mp4" }),
       ],
     });
   },
