5632d04a4232ad5176b16ae83334bf4e3d0d4c81
Toby <toby@vsus.app>
2023-10-07 13:08:24 -0700

    fix: :bug: sao cái nhắc này nó bị bủh r nhỉ :))

--- events/trungBinhMatYeu.ts
+++ events/trungBinhMatYeu.ts
@@ -6,19 +6,17 @@ export default new BotEvent({
   event: Events.MessageCreate,
   async run(message) {
     const folody = message.client as Folody;
-    if (!(await folody.db.get(`${message.author.id}.imBlind`))) return;
-    if (await folody.db.get(`${message.author.id}.blinded`)) return;
+    if (!(await folody.db.get<boolean>(`${message.author.id}.imBlind`))) return;
+    if (await folody.db.get<boolean>(`${message.author.id}.blinded`)) return;
 
     await folody.db.set(`${message.author.id}.blinded`, true);
 
     setTimeout(
       () => {
-        message.channel.send(
-          `${userMention("487597510559531009")} nghỉ mắt đi nào :)`
-        );
+        message.channel.send(`${userMention("487597510559531009")} nghỉ mắt đi nào :)`);
         folody.db.delete(`${message.author.id}.blinded`);
       },
-      1000 * 60 * 20
+      1000 * 60 * 20,
     ); // 20 20 20 rule
   },
 });
