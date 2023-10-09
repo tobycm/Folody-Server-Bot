39291ffdef0d3e818f7b091193cca1f906b790f4
Toby <toby@vsus.app>
2023-10-09 10:43:44 -0700

    fix: :bug:

--- events/coTienThiDonateChoFolodyItThi5mowoNhieuThi50ka.ts
+++ events/coTienThiDonateChoFolodyItThi5mowoNhieuThi50ka.ts
@@ -1,29 +1,23 @@
-import { Events } from "discord.js";
+import { Events, userMention } from "discord.js";
 import BotEvent from "modules/event";
 
-/**
- * @param {Discord.Message} oldMessage
- * @param {Discord.Message} newMessage
- */
+async function sliceNumber(num: string) {
+  const pattern = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(pattern, ",");
+}
+
 export default new BotEvent({
   event: Events.MessageUpdate,
-  async run(oldmessage, newmessage) {
-    if (newmessage.author.id === '408785106942164992') {
-      async function sliceNumber(num) {
-        const pattern = /\B(?=(\d{3})+(?!\d))/g
-        return num.toString().replace(pattern, ',')
-      }
+  async run(oldMessage, newMessage) {
+    if (newMessage.author?.id !== "408785106942164992") return;
+    if (!newMessage.content) return;
+    newMessage.content = newMessage.content.replaceAll("**", "");
 
-      const msg = newmessage.content.split(' ')
-      if (!newmessage.content) return
-      else if (msg[7] === '**<@926643835419910184>**!')
-        return newmessage.channel.send(
-          `> <a:nhoamnhoam:1145729266517016606> cảm ơn bạn ${
-            (msg[2]).replace('**', '')
-          } đã donate cho bọn mình **_${
-            sliceNumber(((msg[4]).replace('**', '')))
-          }_ cowoncy** nha!`
-        );
-    }
+    const msg = newMessage.content.split(" ");
+
+    // akaix
+    if (msg[7] !== userMention("926643835419910184")) return;
+
+    return newMessage.channel.send(`> <a:nhoamnhoam:1145729266517016606> cảm ơn bạn ${msg[2]} đã donate cho bọn mình **_${msg[4]}_ cowoncy** nha!`);
   },
 });
